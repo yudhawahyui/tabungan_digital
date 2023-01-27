53a040b06b94e9c42ef65a392a4e8f5ccdc8f038
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2023-01-28 01:17:15 +0700

    Authentification Android Using email & password

--- lib/app/utils/widget/register_page_widget/register_page_widget_2.dart
+++ lib/app/utils/widget/register_page_widget/register_page_widget_2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tabungan_digital/app/utils/style/AppColors.dart';
+import 'package:tabungan_digital/pages/auth.dart';
 
 class registerPage2 extends StatelessWidget {
   const registerPage2({
@@ -9,6 +10,9 @@ class registerPage2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var emailController = TextEditingController();
+    var passwordController = TextEditingController();
+
     return Padding(
       padding: const EdgeInsets.all(32),
       child: Column(
@@ -39,13 +43,14 @@ class registerPage2 extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
+                  children: [
                     Text('Email'),
                     SizedBox(
                       height: 8,
                     ),
                     TextField(
-                      obscureText: true,
+                      controller: emailController,
+                      obscureText: false,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: 'Masukkan Email',
@@ -61,12 +66,13 @@ class registerPage2 extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
+                  children: [
                     Text('Password'),
                     SizedBox(
                       height: 8,
                     ),
                     TextField(
+                      controller: passwordController,
                       obscureText: true,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
@@ -127,6 +133,9 @@ class registerPage2 extends StatelessWidget {
                 child: TextButton(
                   onPressed: () {
                     // Function Here
+                    AuthController.instance.register(
+                        emailController.text.trim(),
+                        passwordController.text.trim());
                   },
                   child: const Text(
                     'Daftar',
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:tabungan_digital/pages/auth.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+  
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
