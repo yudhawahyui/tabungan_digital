d80f97d3eec6daa0894e168af5857ddc233c63bf
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2023-01-30 21:37:18 +0700

    configure platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:tabungan_digital/firebase_options.dart';
 import 'package:tabungan_digital/pages/auth.dart';
 
 import 'app/routes/app_pages.dart';
@@ -9,7 +10,8 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((value) => Get.put(AuthController()));
 
   runApp(
     GetMaterialApp(
