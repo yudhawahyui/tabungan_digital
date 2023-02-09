e58153050c26ccddbcf04c23b7584bffa547fd1c
yudhawahyui <yudha.iskandar.si.20@cic.ac.id>
2023-02-10 04:07:18 +0700

    Fix bug, add tab, refactoring home page

--- lib/app/modules/home_page/views/home_page_view.dart
+++ lib/app/modules/home_page/views/home_page_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:tabungan_digital/app/modules/detail_tabungan_page/views/detail_tabungan_page_view.dart';
 import 'package:tabungan_digital/app/utils/style/AppColors.dart';
+import 'package:tabungan_digital/app/utils/widget/home_page/targetNabung_widget.dart';
+import 'package:tabungan_digital/app/utils/widget/home_page/targetSelesai_widget.dart';
 import 'package:tabungan_digital/pages/auth.dart';
 import 'package:unicons/unicons.dart';
 
@@ -15,89 +18,125 @@ class HomePageView extends GetView<HomePageController> {
   }) : super(key: key);
   final auth = Get.find<AuthController>();
 
+  List<Tab> myTab = [
+    const Tab(
+      child: Text('Target Tabungan', style: TextStyle(color: AppColors.black)),
+    ),
+    const Tab(
+      child: Text(
+        'Target Selesai',
+        style: TextStyle(color: AppColors.black),
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+      child: DefaultTabController(
+        length: myTab.length,
         child: Scaffold(
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Get.toNamed('/tambah-tabungan-page');
-        },
-        foregroundColor: AppColors.black,
-        backgroundColor: AppColors.white,
-        label: const Text('Tambah Tabungan'),
-        shape: RoundedRectangleBorder(
-          side: const BorderSide(
-            color: AppColors.primaryBg,
-            width: 2,
-          ),
-          borderRadius: BorderRadius.circular(5),
-        ),
-        icon: const Icon(
-          UniconsLine.plus,
-        ),
-      ),
-      body: Container(
-        padding: const EdgeInsets.only(
-          top: 16,
-          bottom: 16,
-          left: 32,
-          right: 32,
-        ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Tabungan Digital',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const Spacer(),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: AppColors.black,
-                  ),
-                  onPressed: () {
-                    auth.logOut();
-                  },
-                  child: const Icon(UniconsLine.sign_out_alt),
-                )
-              ],
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              Get.toNamed('/tambah-tabungan-page');
+            },
+            foregroundColor: AppColors.black,
+            backgroundColor: AppColors.white,
+            label: const Text('Tambah Tabungan'),
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(
+                color: AppColors.primaryBg,
+                width: 2,
+              ),
+              borderRadius: BorderRadius.circular(5),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: AppColors.primaryBg,
+            icon: const Icon(
+              UniconsLine.plus,
+            ),
+          ),
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(130),
+            child: Container(
+              padding: const EdgeInsets.only(
+                left: 32,
+                right: 32,
+                top: 16,
+                bottom: 16,
+              ),
+              width: Get.width * 1,
+              color: AppColors.white,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Text(
+                        'Tabungan Digital',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Spacer(),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          foregroundColor: AppColors.black,
+                        ),
+                        onPressed: () {},
+                        child: const Icon(UniconsLine.sign_out_alt),
+                      )
+                    ],
                   ),
-                  onPressed: () {},
-                  child: const Text('Target Nabung'),
+                  TabBar(
+                    indicatorColor: AppColors.primaryBg,
+                    tabs: myTab,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          body: Container(
+            height: Get.height * .74,
+            child: TabBarView(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 32,
+                    right: 32,
+                  ),
+                  child: GestureDetector(
+                    onTap: () => Get.toNamed('/detail-tabungan-page'),
+                    child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return const TargetNabung();
+                      },
+                    ),
+                  ),
                 ),
                 Container(
-                  width: 1,
-                  height: 20,
-                  color: AppColors.black,
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: AppColors.black,
+                  margin: const EdgeInsets.only(
+                    left: 32,
+                    right: 32,
                   ),
-                  onPressed: () {},
-                  child: const Text('Target Selesai'),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return const TargetSelesai();
+                      },
+                    ),
+                  ),
                 ),
               ],
             ),
-
-            // TargetNabung(),
-            // TargetSelesai()
-          ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
